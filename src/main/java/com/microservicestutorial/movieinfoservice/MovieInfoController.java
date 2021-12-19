fe1e7fd946ab2468d931918131f3e23328556fe4
Mickael Thomere <mickael.thomere@gmail.com>
2021-12-19 15:17:30 +0100

    Microservices Tutorial : Some @Value example + adding doc on properties

--- src/main/java/com/microservicestutorial/movieinfoservice/MovieInfoController.java
+++ src/main/java/com/microservicestutorial/movieinfoservice/MovieInfoController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/movie")
 public class MovieInfoController {
@@ -19,8 +22,20 @@ public class MovieInfoController {
     @Value("${movie-db.api-key}")
     private String apiKey;
 
+    @Value("${my-default-value-variable:defaultValue}")
+    private String defaultValue;
+
+    @Value("${my-list-values-variable}")
+    private List<String> values;
+
+    @Value("#{${my-map-values-variable}}") //the # tells that this is an SpEL expression
+    private Map<String, String> mapValues;
+
     @GetMapping("/{id}")
     public MovieInfoResource getMovieById(@PathVariable(value = "id") int movieId) {
+        System.out.println("default value : " + defaultValue);
+        System.out.println("values : " + values);
+        System.out.println("mapValues : " + mapValues);
         return restTemplate.getForObject("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" +  apiKey, MovieInfoResource.class);
     }
 }
